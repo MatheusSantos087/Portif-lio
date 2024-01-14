6f1800192c466e2ad5088643a96e7c0a2ba26063
Matheus Santos <matheussantosxy@email.com>
2024-01-14 17:01:24 -0300

    primeiro commit

---
+++ main.js
@@ -0,0 +1,15 @@
+import initAnimationScroll from "./Js/animacao-scroll.js"; 
+ import initMenuMobile from "./Js/menu-mobile.js"; 
+ import initMenuTransition from "./Js/menu-transition.js"; 
+ import initScrollSuave from "./Js/scroll-suave.js"; 
+ import initTabNav from "./Js/tabnav.js"; 
+ import initTyping from "./Js/typing.js"; 
+  
+  
+  
+ initTabNav(); 
+ initScrollSuave(); 
+ initAnimationScroll(); 
+ initTyping(); 
+ initMenuTransition(); 
+ initMenuMobile();
